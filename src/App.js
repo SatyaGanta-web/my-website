57257fcadd191fd6a3f52a45b8b9c75c87ee2dea
Swaroop Charan Pasala <debitcard808@gmail.com>
2025-11-02 22:07:28 -0600

    New Commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React, {Component} from "react";
+import Count from "./Count.jsx";
+class App extends Component {
+    render(){
+        return (
+            <div>
+            <Count/>
+            </div>
+        )
+    }
+}
+export default Count;
